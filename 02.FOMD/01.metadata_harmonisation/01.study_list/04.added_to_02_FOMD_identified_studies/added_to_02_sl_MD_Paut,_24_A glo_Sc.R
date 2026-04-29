99d2fec891491b13b65a70ef79f94176f1119913
Andrea Sanchez <andreacsanchezb@gmail.com>
2026-04-29 16:05:26 +0200

    paut list 02

--- 02.FOMD/01.metadata_harmonisation/01.study_list/04.added_to_02_FOMD_identified_studies/added_to_02_sl_MD_Paut,_24_A glo_Sc.R
+++ 02.FOMD/01.metadata_harmonisation/01.study_list/04.added_to_02_FOMD_identified_studies/added_to_02_sl_MD_Paut,_24_A glo_Sc.R
@@ -1,6 +1,6 @@
 library(readxl)
 library(dplyr)
-
+library(tidyr)
 
 path.studylist<-setwd("C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Evidence Hub - General/Agroecology_Evidence_Hub/02.FOMD/01.metadata_harmonisation/01.study_list")
 list.files(path.studylist)
@@ -13,34 +13,53 @@ sl.dic<-read_xlsx(path = "02.selected/sl_MD_Paut,_24_A glo_Sc.xlsx", sheet ="dat
 
 #---Study list data 
 sl.data<-read_xlsx(path = "02.selected/sl_MD_Paut,_24_A glo_Sc.xlsx", sheet="data")
+sl.biblio_info<-read_xlsx(path = "02.selected/sl_MD_Paut,_24_A glo_Sc.xlsx", sheet="biblio_info")
+
          
 #==========================================================
 # Filter included studies only
 #==========================================================
 #sl.data<-sl.data%>%
  # filter(Inclusion_yes_no %in% c("yes","Yes"))
+names(sl.data)
+#==========================================================
+# Match study list with bibliographic information
+#==========================================================
+sl.data.clean<- sl.data%>%
+  select("Id_article" ,	 "Authors","Title" ,"DOI","Year_of_publication")%>%
+  distinct()%>%
+  left_join(sl.biblio_info, by=c("DOI"))
 
 #==========================================================
 # Rename relevant columns
 #==========================================================
-sl.data<-sl.data%>%  
+names(sl.data.clean)
+sl.data<-sl.data.clean%>% 
+  separate_wider_delim(
+    cols = "Pages", 
+    delim = "-", 
+    names = c("start_page", "end_page"),
+    too_many = "merge",        # Combines Mexico and Brazil into 'country3'
+    too_few = "align_start")%>%
   #Rename columns to match with our dataset
   rename(
     "authors"="Authors",
-    #"journal"="",
+    "journal"="Journal Abbreviation",
     #"article_number"="",
     #"start_page"="",
     #"end_page"="",
-    #"issue"="",
-    "title"="Title",
-    #"study_type"="",
-    #"volume"="",
+    "title"="Title.x",
+    "study_type"="Item Type" ,
     "year"="Year_of_publication",
     "doi"="DOI",
-    #"issn"="",
-    "code_from_ss"="Id_article"
+    "code_from_ss"="Id_article",
     #"keywords"=","
-    #"abstract"="",
+    "abstract"="Abstract Note",
+    "url"="Url",
+    "issn"= "ISSN",
+    "volume"="Volume",
+    "issue"="Issue" ,
+    "study_type"="Item Type" 
     
       )
 
@@ -49,18 +68,11 @@ sl.data<-sl.data%>%
 #==========================================================
 sl.data<-sl.data%>%    
   mutate("ss_id"="MD_Paut,_24_A glo_Sc",
-         "journal"=NA,
+         
          "article_number"=NA,
-         "start_page"=NA,
-         "end_page"=NA,
-         "issue"=NA,
          "booktitle"=NA,
-         "url"=NA ,
-         "keywords"=NA,
-         "abstract"=NA,
-         "volume"=NA,
-         "study_type"="JA",
-         "issn"=NA,
+         
+         "keywords"=NA
          )
 
 #==========================================================
@@ -70,8 +82,7 @@ sl.data<-sl.data%>%
   select(ss_id,	authors,journal, booktitle,		article_number,
          start_page,	end_page,	issue,	title,	volume,	year,	doi,
          issn,url,code_from_ss,keywords,	abstract,study_type)%>%
-  mutate(across(everything(), as.character))%>%
-  distinct()
+  mutate(across(everything(), as.character))
 
 sapply(sl.data,class)
 names(sl.data)
