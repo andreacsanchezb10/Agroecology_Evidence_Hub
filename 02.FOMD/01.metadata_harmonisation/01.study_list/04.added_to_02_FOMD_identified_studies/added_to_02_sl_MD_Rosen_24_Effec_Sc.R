87d606c4d707236c1565946ce9a8544700c1b158
Andrea Sanchez <andreacsanchezb@gmail.com>
2026-07-22 20:01:53 +0200

    55

--- 02.FOMD/01.metadata_harmonisation/01.study_list/04.added_to_02_FOMD_identified_studies/added_to_02_sl_MD_Rosen_24_Effec_Sc.R
+++ 02.FOMD/01.metadata_harmonisation/01.study_list/04.added_to_02_FOMD_identified_studies/added_to_02_sl_MD_Rosen_24_Effec_Sc.R
@@ -1,58 +1,113 @@
 library(readxl)
 library(dplyr)
+library(tidyr)
 
-setwd("C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Knowledge Hub - General/Agroecology_Knolwedge_Hub/03.FOMD_datasets/FOMD_study_list/")
+path.study.list<- "C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Evidence Hub - General/Agroecology_Evidence_Hub/02.FOMD/01.metadata_harmonisation/01.study_list"
+path.study.metadata<- "C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Evidence Hub - General/Agroecology_Evidence_Hub/02.FOMD/01.metadata_harmonisation/02.metadata"
 
-# Article list data dictionary
-#sl.dic<-read_xlsx(path = "datasets/pa_article_list/pending/", sheet ="")
 
-# Article list data dictionary
-sl.data<-read.csv("02_selected/sl_MD_Rosen_24_Effec_Sc.csv",header = TRUE, sep = ",")
+list.files(path.study.list)
+list.files(path.study.metadata)
 
+#==========================================================
+# Read datasets
+#==========================================================
+#---Study list data 
+sl.data<-read.csv(file.path(path.study.list,"/02.selected/sl_MD_Rosen_24_Effec_Sc.csv"),header = TRUE, sep = ",")
+sl.data1<-read.csv(file.path(path.study.list,"/02.selected/sl_MD_Rosen_24_Effec_Sc_completed.csv"),header = TRUE, sep = ",")
+
+#---Metadata
+keep.list <- read.csv(
+  file.path(path.study.metadata, "/04.added_to_06_FOMD_metadata_original_long/added_to_10_MD_Rosen_24_Effec_Sc.csv"),
+  header = TRUE, sep = ",")
+keep_ids <- unique(keep.list$study_id %>% as.character() %>% trimws())
+
+
+# ---- Source 1: sl_MD_Rosen_24_Effec_Sc.csv ----
+#with complete bibliographic information but not all included papers
 sl.data<-sl.data%>%
-  tidyr::separate(col="PAGES", into= c("start_page","end_page"), sep="--", remove=TRUE, convert=TRUE)%>%
-  mutate(CATEGORY=case_when(
-    CATEGORY=="ARTICLE"~"article",
-    CATEGORY=="INCOLLECTION"~"incollection",
-  ))%>%
-  
+  separate(
+    col="PAGES", into= c("start_page","end_page"), 
+    sep="--", remove=TRUE, convert=TRUE)%>%
   mutate(TYPE=case_when(
     TYPE=="Journal Article"~"JA",
     TYPE=="Serial"~"S",
     TRUE~NA
   ))%>%
-  
-  #Rename columns to match with our dataset
-  dplyr::rename(
-    "category"="CATEGORY",
-    "authors"="AUTHOR",
-    "booktitle"="BOOKTITLE",
-    "journal"="JOURNAL",
-    "article_number"="NUMBER",
-    #"start_page"="start_page,
-    #"end_page"="end_page,
-    #"issue"	=,
-    "title"="TITLE",
-    "study_type"="TYPE",
-    "volume"="VOLUME",
-    "year"="YEAR",
-    "doi"="DOI",
-    "issn"="ISSN",
-    "url"="URL",
-    "code_from_ss"="ERACODE",
-    "keywords"="KEYWORDS",
-    "abstract"="ABSTRACT"
-  )%>%
-  #Complete the source code
+  rename(
+    category       = CATEGORY,
+    authors        = AUTHOR,
+    booktitle      = BOOKTITLE,
+    journal        = JOURNAL,
+    article_number = NUMBER,
+    title          = TITLE,
+    study_type     = TYPE,
+    volume         = VOLUME,
+    year           = YEAR,
+    doi            = DOI,
+    issn           = ISSN,
+    url            = URL,
+    code_from_ss   = ERACODE,
+    keywords       = KEYWORDS,
+    abstract       = ABSTRACT
+  ) %>%
   mutate("ss_id"="MD_Rosen_24_Effec_Sc",
-         "issue"=NA)%>%
+         "issue"=NA_character_)%>%
   
   select(ss_id,	authors,journal, booktitle,		article_number,
          start_page,	end_page,	issue,	title,	volume,	year,	doi,
          issn,url,code_from_ss,keywords,	abstract,study_type)%>%
-  mutate(across(everything(), as.character))
+  mutate(across(everything(), as.character))%>%
+  distinct()
 
-sapply(sl.data,class)
-names(sl.data)
 
-writexl::write_xlsx(sl.data,"04_added_to_04_FOMD_identified_studies/added_to_04_sl_MD_Rosen_24_Effec_Sc.xlsx")
+
+# ---- Source 2: sl_MD_Rosen_24_Effec_Sc_completed.csv ----
+sl.data1<-sl.data1%>%
+  # 1. Rename CSV columns to match the target (xlsx) schema
+  #    Verified via join on B.Code == code_from_ss (950 overlapping studies)
+  rename(
+    "code_from_ss" = "B.Code",
+    "authors"      = "B.Author.Last",
+    "year"         = "B.Date",
+    "journal"      = "B.Journal",
+    "doi"          = "B.DOI",
+    "url"          = "B.Url"
+  )%>%
+  select(code_from_ss,authors,year,journal,doi,url)%>%
+  mutate("ss_id"="MD_Rosen_24_Effec_Sc",
+         "study_type"="JA")
+
+# ---- Merge / update sl.data with completed info ----
+# Use code_from_ss as the join key; completed values overwrite base values where present
+sl.data.completed<-sl.data1%>%
+  left_join(
+    sl.data, #%>% select(code_from_ss, authors, year, journal, doi, url),
+    by = "code_from_ss",
+    suffix = c("", "_completed")
+  )%>% 
+  mutate(
+    authors = coalesce(as.character(authors_completed), as.character(authors)),
+    journal = coalesce(as.character(journal_completed), as.character(journal)),
+    year    = coalesce(as.character(year_completed), as.character(year)),
+    doi     = coalesce(as.character(doi_completed), as.character(doi)),
+    url     = coalesce(as.character(url_completed), as.character(url))
+  ) %>%
+  select(-ends_with("_completed"))%>%
+  
+  select(ss_id,	authors,journal, booktitle,		article_number,
+         start_page,	end_page,	issue,	title,	volume,	year,	doi,
+         issn,url,code_from_ss,keywords,	abstract,study_type)
+  
+names(sl.data.completed)
+
+
+# ---- Filter to only studies in the "added_to" metadata ----
+sl.data.completed$code_from_ss <- trimws(as.character(sl.data.completed$code_from_ss))
+
+matched    <- sl.data.completed %>% filter(code_from_ss %in% keep_ids)
+unmatched_ids <- setdiff(keep_ids, sl.data.completed$code_from_ss)
+
+
+writexl::write_xlsx(sl.data.completed,
+                    paste0(path.study.list,"/04.added_to_02_FOMD_identified_studies/added_to_02_sl_MD_Rosen_24_Effec_Sc.xlsx"))
--- 02.FOMD/01.metadata_harmonisation/01.study_list/05.added_to_04_FOMD_screening/added_to_04_sl_MD_Rosen_24_Effec_Sc.R
+++ 02.FOMD/01.metadata_harmonisation/01.study_list/05.added_to_04_FOMD_screening/added_to_04_sl_MD_Rosen_24_Effec_Sc.R
@@ -1,70 +1,118 @@
 library(readxl)
 library(dplyr)
-library(stringr)
+library(tidyr)
 
-setwd("C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Knowledge Hub - General/Agroecology_Knolwedge_Hub/03.FOMD_datasets/FOMD_study_list/")
+path.study.list<- "C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Evidence Hub - General/Agroecology_Evidence_Hub/02.FOMD/01.metadata_harmonisation/01.study_list"
+path.study.metadata<- "C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Evidence Hub - General/Agroecology_Evidence_Hub/02.FOMD/01.metadata_harmonisation/02.metadata"
 
-#-----------------------------
-# Helpers
-#-----------------------------
-norm_txt <- function(x) {
-  x %>%
-    as.character() %>%
-    str_to_lower() %>%
-    str_squish() %>%
-    na_if("")  # turn "" into NA
-}
 
-make_key_ty <- function(title, authors) {
-  t <- norm_txt(title)
-  y <- norm_txt(authors)
-  ifelse(is.na(t) | is.na(y), NA_character_, paste0(t, " || ", y))
-}
+list.files(path.study.list)
+list.files(path.study.metadata)
 
-#-----------------------------
+#==========================================================
 # Read datasets
-#-----------------------------
-FOMD_identified_studies <- read_xlsx(
-  path  = "C:/Users/andreasanchez/OneDrive - CGIAR/Alliance-Agroecology Knowledge Hub - General/Agroecology_Knolwedge_Hub/02.FOMD_structure/04_FOMD_identified_studies.xlsx",
-  sheet = "04_FOMD_identified_studies")
+#==========================================================
+#---Study list data 
+sl.data<-read.csv(file.path(path.study.list,"/02.selected/sl_MD_Rosen_24_Effec_Sc.csv"),header = TRUE, sep = ",")
+sl.data1<-read.csv(file.path(path.study.list,"/02.selected/sl_MD_Rosen_24_Effec_Sc_completed.csv"),header = TRUE, sep = ",")
 
-#-----------------------------
-# Build MD_Rosen_24_Effec_Sc
-#-----------------------------
-MD_Rosen_24_Effec_Sc <- FOMD_identified_studies %>%
-  filter(ss_id == "MD_Rosen_24_Effec_Sc") %>%
+#---Metadata
+keep.list <- read.csv(
+  file.path(path.study.metadata, "/04.added_to_06_FOMD_metadata_original_long/added_to_10_MD_Rosen_24_Effec_Sc.csv"),
+  header = TRUE, sep = ",")
+keep_ids <- unique(keep.list$study_id %>% as.character() %>% trimws())
+
+
+# ---- Source 1: sl_MD_Rosen_24_Effec_Sc.csv ----
+#with complete bibliographic information but not all included papers
+sl.data<-sl.data%>%
+  separate(
+    col="PAGES", into= c("start_page","end_page"), 
+    sep="--", remove=TRUE, convert=TRUE)%>%
+  mutate(TYPE=case_when(
+    TYPE=="Journal Article"~"JA",
+    TYPE=="Serial"~"S",
+    TRUE~NA
+  ))%>%
+  rename(
+    category       = CATEGORY,
+    authors        = AUTHOR,
+    booktitle      = BOOKTITLE,
+    journal        = JOURNAL,
+    article_number = NUMBER,
+    title          = TITLE,
+    study_type     = TYPE,
+    volume         = VOLUME,
+    year           = YEAR,
+    doi            = DOI,
+    issn           = ISSN,
+    url            = URL,
+    code_from_ss   = ERACODE,
+    keywords       = KEYWORDS,
+    abstract       = ABSTRACT
+  ) %>%
+  mutate("ss_id"="MD_Rosen_24_Effec_Sc",
+         "issue"=NA_character_)%>%
+  
+  select(ss_id,	authors,journal, booktitle,		article_number,
+         start_page,	end_page,	issue,	title,	volume,	year,	doi,
+         issn,url,code_from_ss,keywords,	abstract,study_type)%>%
+  mutate(across(everything(), as.character))%>%
+  distinct()
+
+
+
+# ---- Source 2: sl_MD_Rosen_24_Effec_Sc_completed.csv ----
+sl.data1<-sl.data1%>%
+  # 1. Rename CSV columns to match the target (xlsx) schema
+  #    Verified via join on B.Code == code_from_ss (950 overlapping studies)
+  rename(
+    "code_from_ss" = "B.Code",
+    "authors"      = "B.Author.Last",
+    "year"         = "B.Date",
+    "journal"      = "B.Journal",
+    "doi"          = "B.DOI",
+    "url"          = "B.Url"
+  )%>%
+  select(code_from_ss,authors,year,journal,doi,url)%>%
+  mutate("ss_id"="MD_Rosen_24_Effec_Sc",
+         "study_type"="JA")
+
+# ---- Merge / update sl.data with completed info ----
+# Use code_from_ss as the join key; completed values overwrite base values where present
+sl.data.completed<-sl.data1%>%
+  left_join(
+    sl.data, #%>% select(code_from_ss, authors, year, journal, doi, url),
+    by = "code_from_ss",
+    suffix = c("", "_completed")
+  )%>% 
   mutate(
-    status = NA,
-    exclusion_reason = NA,
-    screen_person = NA,
-    screen_date = NA,
-    study_id = NA,
-    key_ty = make_key_ty(title, authors)
-  )
+    authors = coalesce(as.character(authors_completed), as.character(authors)),
+    journal = coalesce(as.character(journal_completed), as.character(journal)),
+    year    = coalesce(as.character(year_completed), as.character(year)),
+    doi     = coalesce(as.character(doi_completed), as.character(doi)),
+    url     = coalesce(as.character(url_completed), as.character(url))
+  ) %>%
+  select(-ends_with("_completed"))%>%
+  
+  select(ss_id,	authors,journal, booktitle,		article_number,
+         start_page,	end_page,	issue,	title,	volume,	year,	doi,
+         issn,url,code_from_ss,keywords,	abstract,study_type)%>%
+  mutate(status="I",
+         exclusion_reason="",
+         screen_person="ERA team",
+         screen_date= "",
+         study_id= "",
+         country= "",
+         RESPONSIBLE= ""
+)
 
-#-----------------------------
-# Deduplicate
-#-----------------------------
-# 1️⃣ First remove duplicates where DOI exists
-step1 <- MD_Rosen_24_Effec_Sc %>%
-  filter(!is.na(doi)) %>%
-  distinct(doi, .keep_all = TRUE)
+# ---- Filter to only studies in the "added_to" metadata ----
+sl.data.completed$code_from_ss <- trimws(as.character(sl.data.completed$code_from_ss))
 
-# 2️⃣ Now handle records without DOI
-no_doi <- MD_Rosen_24_Effec_Sc %>%
-  filter(is.na(doi))
+matched    <- sl.data.completed %>% filter(code_from_ss %in% keep_ids)
+unmatched_ids <- setdiff(keep_ids, sl.data.completed$code_from_ss)
 
-step2 <- no_doi %>%
-  filter(!is.na(key_ty)) %>%
-  distinct(key_ty, .keep_all = TRUE)
 
-# 3️⃣ Keep records that have neither DOI nor key
-step3 <- no_doi %>%
-  filter(is.na(key_ty))
-
-# 4️⃣ Combine all
-deduplicated.MD_Rosen_24_Effec_Sc <- bind_rows(step1, step2, step3) %>%
-  select(-key_ty)
-
-writexl::write_xlsx(deduplicated.MD_Rosen_24_Effec_Sc,"05_added_to_06_FOMD_screening/added_to_06_sl_MD_Rosen_24_Effec_Sc.xlsx")
-
+writexl::write_xlsx(sl.data.completed,
+                    paste0(path.study.list,"/05.added_to_04_FOMD_screening/added_to_04_sl_MD_Rosen_24_Effec_Sc.xlsx"))
